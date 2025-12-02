e74bca44f6405de094761ea5248aac8e6f261096
BREW <81044362+b-re-w@users.noreply.github.com>
2025-12-02 09:04:44 +0900

    Feat: Attack Defence Method 2

--- contracts/NativeBank.sol
+++ contracts/NativeBank.sol
@@ -4,17 +4,22 @@ pragma solidity ^0.8.28;
 
 contract NativeBank {
     mapping(address => uint256) public balanceOf;
+    bool private lock;  // 재진입 방지용 락 변수
 
     function withdraw(uint256 amount) external {
+        require(!lock, "is now working on");  // (방법 2) 재진입 방지: 함수 시작 시점에 락 설정
+        lock = true;
+
         uint256 balance = balanceOf[msg.sender];
         require(balance > 0, "insufficient balance");
 
-        balanceOf[msg.sender] = 0;  // (방법 1) 상태 변경: 재진입 공격 방어
+        //balanceOf[msg.sender] = 0;  // (방법 1) 상태 변경: 재진입 공격 방어
 
         (bool success, ) = msg.sender.call{value: amount}("");  // 호출자에게 제어권을 이동 (재진입 가능!)
         require(success, "failed to send native token");
 
-        //balanceOf[msg.sender] = 0;  // 재진입 방지를 위해 위치 이동
+        balanceOf[msg.sender] = 0;  // 재진입 방지를 위해 위치 이동
+        lock = false;  // (방법 2) 재진입 방지: 함수 종료 시점에 락 해제
 
         //require(balanceOf[msg.sender] >= amount, "Insufficient balance");
         ////payable(msg.sender).transfer(amount);
